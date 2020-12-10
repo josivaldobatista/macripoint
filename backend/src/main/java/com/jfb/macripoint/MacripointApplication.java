eda2007daafc4d5f2f30ce3d76f9ee09544ef167
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-12-09 22:52:44 -0300

    Project created.(OK)

---
+++ backend/src/main/java/com/jfb/macripoint/MacripointApplication.java
@@ -0,0 +1,13 @@
+package com.jfb.macripoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacripointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacripointApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jfb/macripoint/MacripointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfb.macripoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacripointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
