7e97bd9af1853c98be3a7162cf9b2091572a0508
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-12-10 22:06:44 -0300

    Delete Category.

--- backend/src/main/java/com/jfb/macripoint/services/CategoryService.java
+++ backend/src/main/java/com/jfb/macripoint/services/CategoryService.java
@@ -9,9 +9,12 @@ import javax.persistence.EntityNotFoundException;
 import com.jfb.macripoint.dto.CategoryDTO;
 import com.jfb.macripoint.entities.Category;
 import com.jfb.macripoint.repository.CategoryRepository;
+import com.jfb.macripoint.services.exceptions.DatabaseException;
 import com.jfb.macripoint.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -52,6 +55,16 @@ public class CategoryService {
       return new CategoryDTO(entity);
     } catch (EntityNotFoundException e) {
       throw new ResourceNotFoundException("ID não encontrado " + id);
+    }
+  }
+
+  public void delete(Long id) {
+    try {
+      repository.deleteById(id);
+    } catch (EmptyResultDataAccessException e) {
+      throw new ResourceNotFoundException("ID não encontrado " + id);
+    } catch (DataIntegrityViolationException e) {
+      throw new DatabaseException("Violação de integridade!");
     }
   }
 
